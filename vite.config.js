de926f3ac4144797993ff5ec862302643c5eab02
toby <tobyfoster97@gmail.com>
2025-02-11 17:11:17 +0100

    experimen 1

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,3 @@
-import fs from "fs-extra";
-import path from "path";
-import { defineConfig } from "vite";
 export default {
   base: "./",
-  plugins: [
-    {
-      name: "rename-index-html",
-      closeBundle() {
-        const oldPath = path.resolve(__dirname, "dist", "index.html");
-        const newPath = path.resolve(__dirname, "dist", "main.html");
-
-        fs.renameSync(oldPath, newPath);
-      },
-    },
-  ],
 };
