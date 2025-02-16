c9159eec6629da87706d60d07cec1de6833a3151
toby <tobyfoster97@gmail.com>
2025-02-16 15:07:26 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,24 @@
-export default {
+import { defineConfig } from 'vite';
+import purgecss from 'vite-plugin-purgecss';
+
+export default defineConfig({
   base: "./",
-};
+  plugins: [
+    purgecss({
+      content: ['./index.html', './src/**/*.js'],
+      safelist: {
+        standard: [
+          /selectize-.*/, 
+          'item-column', 'mb-4', 'col-12', 'col-sm-6', 'col-md-4', 'col-lg-4', 'col-xl-4'
+        ],
+      },
+    }),
+  ],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "bootstrap/scss/bootstrap";`,
+      },
+    },
+  },
+});
